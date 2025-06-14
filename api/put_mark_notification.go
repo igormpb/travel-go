93c86bb0054222b02026552fd11902dd09742a48
igormpb <igor@hotmail.com>
2025-06-14 16:38:12 -0300

    changes

--- api/put_mark_notification.go
+++ api/put_mark_notification.go
@@ -6,8 +6,8 @@ func (srv *Service) MarkNotificationRead(c *fiber.Ctx) error {
 	id := c.Params("id")
 
 	if err := srv.repository.MarkNotificationAsRead(id); err != nil {
-		return c.Status(500).JSON(fiber.Map{"error": "Failed to mark as read"})
+		return c.Status(500).JSON(fiber.Map{"error": "Falha ao marcar como lida"})
 	}
 
-	return c.JSON(fiber.Map{"message": "Notification marked as read"})
+	return c.JSON(fiber.Map{"message": "Notificação marcada como lida"})
 }
